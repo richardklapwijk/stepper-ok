4edf886a085de6d638a11f2cd0ccd1d27a49049b
richardklapwijk <110915049+richardklapwijk@users.noreply.github.com>
2022-11-05 23:12:38 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -34,3 +34,38 @@ basic.forever(function () {
         }
     }
 })
+// Motor 1 Uses Pins 13, 14, 15 & 16 P13 - Blue P14 - Purple/Pink P15 - Yellow P16 - Orange Driven by a 5volt ULN2003 Darlington Array Motor 2 Uses Pins 6, 7, 9 & 10 P6 - Blue P7 - Purple/Pink P9 - Yellow P10 - Orange Driven by a 5volt ULN2003 Darlington Array
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        Pause = 5
+        for (let index = 0; index < 512; index++) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.digitalWritePin(DigitalPin.P3, 1)
+            basic.pause(Pause)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            pins.digitalWritePin(DigitalPin.P3, 0)
+            basic.pause(Pause)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.digitalWritePin(DigitalPin.P3, 0)
+            basic.pause(Pause)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.digitalWritePin(DigitalPin.P3, 0)
+            basic.pause(Pause)
+        }
+    }
+})
